656dfb0629697c9d6a482913a1c26c31a3a0f58f
Jae Bradley <jae.b.bradley@gmail.com>
2023-03-01 22:32:01 -0500

    Initial softball script (#5)

--- email/addresses/validate.sh
+++ email/addresses/validate.sh
@@ -3,10 +3,13 @@
 function validate() {
   if [[ "1" != "$#" ]]; then printf "Expected exactly one argument, an email address\n" && exit 255; fi
   local -r email_address="$1"
-  local result
+
+  type grep > /dev/null 2>&1
+  if [[ $? -ne 0 ]]; then printf "Unable to identify the grep command\n" && exit 255; fi
+
   # Regex from https://fightingforalostcause.net/content/misc/2006/compare-email-regex.php
-  result=$(grep --quiet --extended-regexp --regexp="^[-a-z0-9~grep%^&*_=+}{\'?]+(\.[-a-z0-9~grep%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$" <<< "${email_address}")
-  if [[ "0" != "$?" ]]; then printf "${email_address} is an invalid email address\n" && exit 255; fi
+  grep --quiet --extended-regexp --regexp="^[-a-z0-9~grep%^&*_=+}{\'?]+(\.[-a-z0-9~grep%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$" <<< "${email_address}"
+  if [[ $? -ne 0 ]]; then printf "Unable to validate email address: ${email_address}\n" && exit 255; fi
 }
 
 validate "$@"
