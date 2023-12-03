8085dde02b59120e994b292ba16a2effe97ceebc
Jae Bradley <jae.b.bradley@gmail.com>
2023-12-03 10:36:13 -0500

    Twelvedata stock price identifier (#7)

---
+++ twelvedata/check instrument price.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function main() {
+  if [[ "2" != "$#" ]]; then printf "Expected two arguments, the instrument identifier (e.g. a stock ticker) and an API key\n" && exist 255; fi
+
+  $(command -v "curl" &> /dev/null)
+  if [[ "0" != "$?"  ]]; then printf "Unable to identify curl program\n" && exit 255; fi
+
+  local -r instrument_identifier="$1"
+  local -r api_key="$2"
+
+  curl --silent --request GET --url "https://api.twelvedata.com/time_series?apikey=${api_key}&interval=1min&symbol=${instrument_identifier}&format=CSV&dp=2"  | awk -F ";" 'NR==2 { print $2 }'
+}
+
+main "$@"
